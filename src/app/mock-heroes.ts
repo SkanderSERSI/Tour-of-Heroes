690ca09d703e175565f1d76037ae7914309afafe
SkanderSERSI <sersi.skander077@gmail.com>
2020-10-01 15:42:29 +0200

    advanced

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,11 @@
+import { Hero } from './Hero';
+
+export const HEROES: Hero[] = [
+  { id: 11, name: 'Dr Nice' },
+  { id: 12, name: 'Narco' },
+  { id: 13, name: 'Bombasto' },
+  { id: 14, name: 'Celeritas' },
+  { id: 15, name: 'Magneta' },
+  { id: 16, name: 'Dr IQ' },
+  { id: 17, name: 'Dr Mustache' }
+];
